878c0b6e6a86d0fe6cbc131c4010bba80bf8294b
Rajesha-Jagadeesh <162010794+Rajesha-Jagadeesh@users.noreply.github.com>
2024-03-05 22:42:46 +0530

    added cros

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import router from "./Router/Router.js";
-
+import cros from "cros";
 const app = express();
-// app.use(cros());
+app.use(cros({
+  origin: 'https://easy-shopping-sandbox.web.app',
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
 app.use('/api/v1', router)
 app.use("*", (req, res)=>{
